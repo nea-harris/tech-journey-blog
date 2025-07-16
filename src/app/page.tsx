90b84395937a1f419086922e9d7b4889fe769d19
Luke Dunscombe <dunscombe1@gmail.com>
2025-07-16 18:44:30 +0000

    War of the dum dums

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ type AllPostsData = {
 }[]
 
 // Change this url to change the image!
-const imageUrl = '/images/eagle.jpg';
+const imageUrl = '/images/snorlax.jpg';
 
 export default function Home() {
   const allPostsData: AllPostsData = getSortedPostsData()
