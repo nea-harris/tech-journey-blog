4a8e1308da2a073a0421eaf09927970e9d23cd7f
nea-harris <peculiarchildren2@gmail.com>
2025-07-16 20:30:07 +0000

    deleted rick and morty

--- next.config.js
+++ next.config.js
@@ -13,10 +13,6 @@ const nextConfig = {
             {
                 protocol: 'https',
                 hostname: 'raw.githubusercontent.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'https://images.unsplash.com/photo-1618440111263-e913c9f5f56d?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
             }
         ],
     },
--- src/components/NavBarContainer.tsx
+++ src/components/NavBarContainer.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Link from "next/link";
 import type {UrlObject} from "url";
 import Image from "next/image";
-import profilePic from '../../public/images/avatar.jpg'
 import DarkModeButton from "@/components/DarkModeButton";
 
 interface NavBarContainerProps {
@@ -15,7 +14,7 @@ interface NavBarContainerProps {
 }
 
 const NavBarContainer: React.FC<NavBarContainerProps> = ({title, children, links}) => {
-  const profileImage = 'https://images.unsplash.com/photo-1669050589643-a06e01d7391e'
+  const profileImage = 'https://images.unsplash.com/photo-1618440111263-e913c9f5f56d?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
   function renderLinks() {
     return links?.map((l, i) => {
       return (
