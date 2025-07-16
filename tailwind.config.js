9f0f5fe9b2c92e8ccb7e42f3803a8e6955e542ad
Luke Dunscombe <dunscombe1@gmail.com>
2025-07-16 19:51:58 +0000

    changing theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     daisyui: {
         themes: [
-            "luxury",
+            "fantasy",
             {
                 "mytheme": {
                     "primary": "#14b8a6",
